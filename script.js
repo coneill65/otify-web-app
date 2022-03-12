d3b9b672905d0275669937d89fa608fd8de62cb2
colin oneill <coneill65@students.mchenry.edu>
2022-03-12 13:45:48 -0600

    added all the code!

---
+++ script.js
@@ -0,0 +1,7 @@
+function fun01(){
+    document.getElementById("song-controlls").src = "songs/Arc North - Never Gonna (Radio Edit) (Official Audio)-E-P_OaIFyCc.mp3"
+}
+function fun02(){
+    songsDiv = document.getElementById("songsdiv")
+    songsDiv.innerHTML += "<p>Hello World</p>";
+}
